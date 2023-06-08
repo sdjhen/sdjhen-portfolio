f329111191fa31d21b6afc09f31dfb6e98bd4bc5
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-06-09 00:58:42 +0100

    added typography, logo & navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 
-function App() {
+const App = () => {
   
-
   return (
     <div>
-      
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Navbar />
+     
+    
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logo-white.png'
 
-
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>
-       
-    </div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#000] text-gray-300'>
+      <div>
+      <img src={Logo} alt="Logo Image" style={{width: '80px'}} />
+      </div>
+
+      {/* Nav Menu */}
+      <div>
+        <ul className='flex'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* Hamburger */}
+          <div className='hidden'>
+            <FaBars />
+          </div>
+          
+          
+          {/* Mobile Menu */}
+          <ul className='hidden'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+          </ul>
+          
+          
+          {/* Social Icons */}
+          <div className='hidden'></div>
+    </div> 
   )
 }
+
+
 
 export default Navbar

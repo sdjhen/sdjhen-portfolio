ecd2d67bdc7ee5521a5ea21e6d9b92d33c9ecd9c
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-06-14 20:22:15 +0100

    added button to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,12 @@ const Contact = () => {
         <form action="" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>// Submit the form below or drop me an email</p>
+                <p className='text-gray-300 py-4'>// Submit the form below or drop me an email.</p>
             </div>
             <input className='bg-[#ccd6fc] p-2' type="text" placeholder='Name' name='name' />
             <input className='bg-[#ccd6fc] p-2 my-4' type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6fc] p-2' type="text" name='message' rows="10" placeholder='Message Here' />
+            <button className='text-white group border-2 px-4 py-3 my-8 mx-auto flex items-center hover:bg-pink-600 hover:border-pink-600'>Let's Collaborate!</button>
         </form>
     </div>
   )

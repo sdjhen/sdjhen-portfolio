9812bab69f3fc829e7243a8e620e912cd240583f
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-06-14 13:52:38 +0100

    reconfig of about and projects components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         </div>
         <div className='max-w[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className=' sm:text-right text-4xl font-bold '>
-                <p>Hi, I'm Sam, nice to meet you. Please take a look around.</p>
+                <p>Hi, I'm Sam, pleasure to meet you. Please take a look around.</p>
             </div>
             <div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit molestias fugiat accusantium, iure culpa ut quaerat nihil, odio iusto quos vel alias omnis ipsum nesciunt dignissimos facilis repellendus sunt autem voluptatum! Sed velit ea porro, sunt fugiat similique repudiandae recusandae.</p>

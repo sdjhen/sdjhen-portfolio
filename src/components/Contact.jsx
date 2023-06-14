77efb7ca591c74d5f30cb82e8e2a10641715d3cf
sdjhen <114769817+sdjhen@users.noreply.github.com>
2023-06-15 00:56:45 +0100

    MVP achieved

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Contact = () => {
+  const getformURL = import.meta.env.VITE_GETFORM_URL;
+
   return (
     <div name="name" className='w-full h-screen bg-[#000] flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action={getformURL} className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or drop me an email.</p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import WorkImg from '../assets/projects/workImg.jpeg'
 import realEstate from '../assets/projects/realestate.jpg'
-import Firebase from '../assets/firebase.png'
 
 const Projects = () => {
   return (
